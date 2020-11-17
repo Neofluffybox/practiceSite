1f0ec8618c0a2f49d5cac0d1b124c0f15397f1ea
Nicholas Watson <neonicc95@gmail.com>
2020-11-17 16:08:53 -0700

    testing using VS code to make commit message

--- scripts/scripty.js
+++ scripts/scripty.js
@@ -1,39 +1,74 @@
-const header = document.querySelector(`header`);
-header.classList.add(`container`);
+const rps = document.querySelector('.rps');
 
-const container = document.querySelector(`.container`);
+const btn1 = document.querySelector('#one');
+const btn2 = document.querySelector('#three');
+const btn3 = document.querySelector('#five');
+const choice = document.querySelector('.rps');
+const btnr = document.createElement('input');
+btnr.setAttribute('type', 'button');
+btnr.setAttribute('id', 'rock');
+btnr.setAttribute('name', 'rock');
+const btnrlabel = document.createElement('label');
+btnrlabel.setAttribute('for', 'rock');
+btnrlabel.innerHTML = 'Rock';
+const btnp = document.createElement('input');
+btnp.setAttribute('type', 'button');
+btnp.setAttribute('id', 'paper');
+btnp.setAttribute('name', 'paper');
+const btns = document.createElement('input');
+btns.setAttribute('type', 'button');
+btns.setAttribute('id', 'scissors');
+btns.setAttribute('name', 'scissors');
+function game(outOf) {
+    for (i = 1; i <= outOf; i++) {
+        let intro = document.querySelector('.round');
+        intro.innerHTML = 'Round: ' + i;
+        let choice;
+        let options = ["rock", "paper", "scissors"];
+        let rchoice = Math.floor(Math.random() * options.length);
+        let player1 = choice.toLowerCase();
+        let player2 = options[rchoice];
+        alert("computer chose " + player2 + "!");
+        if (player1 === player2) {
+            alert("it's a tie!");
+        } else if (player1 == "rock") {
+            if (player2 == "paper") {
+                alert("you lose!");
+            } else if (player2 == "scissors") {
+                alert("you win!");
+            } else {
+                alert("no game");
+            };
+        } else if (player1 == "paper") {
+            if (player2 == "rock") {
+                alert("you win!");
+            } else if (player2 == "scissors") {
+                alert("you lose!");
+            } else {
+                alert("no game");
+            };
+        } else if (player1 == "scissors") {
+            if (player2 == "rock") {
+                alert("you lose!");
+            } else if (player2 == "paper") {
+                alert("you win!");
+            } else {
+                alert("no game");
+            };
+        } else {
+            alert("invalid input");
+        };
+    }
+}
 
-const content = document.createElement(`div`);
+btn1.addEventListener('click', () => {
+    game(1);
+});
 
-content.textContent = `hello world`;
+btn2.addEventListener('click', () => {
+    game(3);
+});
 
-container.appendChild(content)
-
-const article = document.querySelector('article');
-
-const paragraph = document.createElement('p');
-
-paragraph.style.color = 'red';
-paragraph.innerHTML = 'Hey, I\'m red!'
-
-article.appendChild(paragraph)
-
-const h3 = document.createElement('h3');
-
-h3.style.color = 'blue';
-
-article.appendChild(h3)
-
-const newDiv = document.createElement('div');
-newDiv.setAttribute('style', 'background-color: pink; border: 2px solid #000');
-
-const h1 = document.createElement('h1');
-h1.innerHTML = 'I\'m in a div';
-
-const paragraph2 = document.createElement('p');
-paragraph2.innerHTML = 'ME TOO';
-
-newDiv.appendChild(h1)
-newDiv.appendChild(paragraph2)
-
-article.appendChild(newDiv)
+btn3.addEventListener('click', () => {
+    game(5);
+});
