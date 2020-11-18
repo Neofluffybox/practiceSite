8d4482a49f75758ca6158e748b19b0bc2c1845a1
Nicholas Watson <neonicc95@gmail.com>
2020-11-18 01:04:34 -0700

    working on ui

--- scripts/scripty.js
+++ scripts/scripty.js
@@ -1,33 +1,31 @@
-const rps = document.querySelector('.rps');
-
 const btn1 = document.querySelector('#one');
 const btn2 = document.querySelector('#three');
 const btn3 = document.querySelector('#five');
-const choice = document.querySelector('.rps');
-const btnr = document.createElement('input');
-btnr.setAttribute('type', 'button');
-btnr.setAttribute('id', 'rock');
-btnr.setAttribute('name', 'rock');
-const btnrlabel = document.createElement('label');
-btnrlabel.setAttribute('for', 'rock');
-btnrlabel.innerHTML = 'Rock';
-const btnp = document.createElement('input');
-btnp.setAttribute('type', 'button');
-btnp.setAttribute('id', 'paper');
-btnp.setAttribute('name', 'paper');
-const btns = document.createElement('input');
-btns.setAttribute('type', 'button');
-btns.setAttribute('id', 'scissors');
-btns.setAttribute('name', 'scissors');
+const btnR = document.querySelector(`#rock`);
+const btnP = document.querySelector('#paper');
+const btnSc = document.querySelector('#scissors');
+const set1 = "#set1";
+
+function displayToggle(set) {
+    var x = document.getElementById(set);
+    if (x.style.display === "none") {
+        x.style.display = `flex`;
+    } else {
+        x.style.display = `none`;
+    };
+}
+
+function compChoice() {
+    let options = ["rock", "paper", "scissors"];
+    let rchoice = Math.floor(Math.random() * options.length);
+    return rchoice
+}
+
 function game(outOf) {
     for (i = 1; i <= outOf; i++) {
         let intro = document.querySelector('.round');
-        intro.innerHTML = 'Round: ' + i;
-        let choice;
-        let options = ["rock", "paper", "scissors"];
-        let rchoice = Math.floor(Math.random() * options.length);
-        let player1 = choice.toLowerCase();
-        let player2 = options[rchoice];
+        intro.innerHTML = `Round: ${i}`;
+        let player2 = compChoice();
         alert("computer chose " + player2 + "!");
         if (player1 === player2) {
             alert("it's a tie!");
@@ -62,13 +60,15 @@ function game(outOf) {
 }
 
 btn1.addEventListener('click', () => {
-    game(1);
+    displayToggle(set1);
 });
 
 btn2.addEventListener('click', () => {
+    displayToggle(set1);
     game(3);
 });
 
 btn3.addEventListener('click', () => {
+    displayToggle(set1);
     game(5);
 });
