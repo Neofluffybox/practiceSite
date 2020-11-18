f1886a416a1e9250246085272dd1d64b0638e6e9
Nicholas Watson <neonicc95@gmail.com>
2020-11-18 05:34:41 -0700

    still trying to configure UI. Using buttons to play game but havent gotten it right yet

--- scripts/scripty.js
+++ scripts/scripty.js
@@ -4,10 +4,19 @@ const btn3 = document.querySelector('#five');
 const btnR = document.querySelector(`#rock`);
 const btnP = document.querySelector('#paper');
 const btnSc = document.querySelector('#scissors');
-const set1 = "#set1";
+let current = 1;
+let pS = 0;
+let cS = 0;
+let cCh = document.querySelector('#cCh');
+let outcome = document.querySelector('#outcome');
+let intro = document.querySelector('.round');
+intro.innerHTML = `Round: ${current}`;
+let score = document.querySelector(`.score`);
+score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
+
 
 function displayToggle(set) {
-    var x = document.getElementById(set);
+    var x = document.querySelector(set);
     if (x.style.display === "none") {
         x.style.display = `flex`;
     } else {
@@ -15,60 +24,47 @@ function displayToggle(set) {
     };
 }
 
-function compChoice() {
-    let options = ["rock", "paper", "scissors"];
-    let rchoice = Math.floor(Math.random() * options.length);
-    return rchoice
-}
-
-function game(outOf) {
-    for (i = 1; i <= outOf; i++) {
-        let intro = document.querySelector('.round');
-        intro.innerHTML = `Round: ${i}`;
-        let player2 = compChoice();
-        alert("computer chose " + player2 + "!");
-        if (player1 === player2) {
-            alert("it's a tie!");
-        } else if (player1 == "rock") {
-            if (player2 == "paper") {
-                alert("you lose!");
-            } else if (player2 == "scissors") {
-                alert("you win!");
-            } else {
-                alert("no game");
-            };
-        } else if (player1 == "paper") {
-            if (player2 == "rock") {
-                alert("you win!");
-            } else if (player2 == "scissors") {
-                alert("you lose!");
-            } else {
-                alert("no game");
-            };
-        } else if (player1 == "scissors") {
-            if (player2 == "rock") {
-                alert("you lose!");
-            } else if (player2 == "paper") {
-                alert("you win!");
-            } else {
-                alert("no game");
-            };
-        } else {
-            alert("invalid input");
-        };
-    }
-}
-
 btn1.addEventListener('click', () => {
-    displayToggle(set1);
+    displayToggle('#set2');
 });
 
 btn2.addEventListener('click', () => {
-    displayToggle(set1);
-    game(3);
+    displayToggle('#set2');
 });
 
 btn3.addEventListener('click', () => {
-    displayToggle(set1);
-    game(5);
+    displayToggle('#set2');
+});
+
+btnR.addEventListener('click', () => {
+    let compSelection = Math.floor(Math.random() * 3);
+    if (compSelection == 0) {
+        cCh.innerHTML = `Comp chose rock too!`;
+    } else if (compSelection == 1) {
+        cCh.innerHTML = 'Comp chose paper!';
+    } else {
+        cCh.innerHTML = 'Comp chose scissors!';
+    };
+});
+
+btnP.addEventListener('click', () => {
+    let compSelection = Math.floor(Math.random() * 3);
+    if (compSelection == 0) {
+        cCh.innerHTML = `Comp chose paper too!`;
+    } else if (compSelection == 1) {
+        cCh.innerHTML = 'Comp chose rock!';
+    } else {
+        cCh.innerHTML = 'Comp chose scissors!';
+    };
+});
+
+btnSc.addEventListener('click', () => {
+    let compSelection = Math.floor(Math.random() * 3);
+    if (compSelection == 0) {
+        cCh.innerHTML = `Comp chose scissors too!`;
+    } else if (compSelection == 1) {
+        cCh.innerHTML = 'Comp chose rock!';
+    } else {
+        cCh.innerHTML = 'Comp chose paper!';
+    };
 });
