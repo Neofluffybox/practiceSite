88a43b54f6cdf4c246fbbda47703120e4c7d7782
Nicholas Watson <neonicc95@gmail.com>
2020-11-19 23:07:03 -0700

    finally finished rock paper scissors game

--- scripts/scripty.js
+++ scripts/scripty.js
@@ -4,14 +4,32 @@ const btn3 = document.querySelector('#five');
 const btnR = document.querySelector(`#rock`);
 const btnP = document.querySelector('#paper');
 const btnSc = document.querySelector('#scissors');
-let current = 1;
+const checkerBtn = document.querySelector('#checker');
+let current = 0;
 let pS = 0;
 let cS = 0;
 let cCh = document.querySelector('#cCh');
 let outcome = document.querySelector('#outcome');
 let intro = document.querySelector('.round');
 let score = document.querySelector(`.score`);
-intro.innerHTML = `Round: ${current}`
+intro.innerHTML = `Round: ${current}`;
+cCh.innerHTML = 'Comp choice will appear here';
+outcome.innerHTML = 'Round outcome here';
+
+function results() {
+    if (pS > cS) {
+        alert('YOU WIN');
+    } else if (pS < cS) {
+        alert('YOU LOSE');
+    } else {
+        alert('IT\'S A TIE');
+    };
+    displayToggle('#set2');
+    pS = 0, cS = 0;
+    cCh.innerHTML = 'Comp choice will appear here';
+    outcome.innerHTML = 'Round outcome here';
+    return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
+}
 
 function compChoice() {
     let options = ['rock', 'paper', 'scissors'];
@@ -27,83 +45,144 @@ function displayToggle(set) {
     };
 }
 
-function roundTicker() {
-    current++;
-    return intro.innerHTML = `Round: ${current}`;
+function setLimit(x) {
+    return limit = x;
 }
 
-function rock() {
-    let comp = compChoice();
-    if (comp == 'rock') {
-        cCh.innerHTML = 'Comp chose rock!';
-        outcome.innerHTML = 'It\'s a tie';
-    } else if (comp == 'paper') {
-        cCh.innerHTML = 'Comp chose paper!';
-        outcome.innerHTML = 'You lose';
-        cS++;
+function roundTicker(z) {
+    if (current < z) {
+        current++;
+        return intro.innerHTML = `Round: ${current}`;
     } else {
-        cCh.innerHTML = 'Comp chose scissors';
-        outcome.innerHTML = 'You win';
-        pS++;
-    };
-    return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
+        current = 0
+        return intro.innerHTML = `Round: ${current}`;
+    }
 }
 
-function paper() {
-    let comp = compChoice();
-    if (comp == 'rock') {
-        cCh.innerHTML = 'Comp chose rock!';
-        outcome.innerHTML = 'You win';
-        pS++;
-    } else if (comp == 'paper') {
-        cCh.innerHTML = 'Comp chose paper!';
-        outcome.innerHTML = 'It\'s a tie';
+function rock(y) {
+    if (current < y) {
+        let comp = compChoice();
+        if (comp == 'rock') {
+            cCh.innerHTML = 'Comp chose rock!';
+            outcome.innerHTML = 'It\'s a tie';
+        } else if (comp == 'paper') {
+            cCh.innerHTML = 'Comp chose paper!';
+            outcome.innerHTML = 'You lose';
+            cS++;
+        } else {
+            cCh.innerHTML = 'Comp chose scissors';
+            outcome.innerHTML = 'You win';
+            pS++;
+        };
+        return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
     } else {
-        cCh.innerHTML = 'Comp chose scissors';
-        outcome.innerHTML = 'You lose';
-        cS++;
+        if (pS > cS) {
+            alert('YOU WIN');
+        } else if (pS < cS) {
+            alert('YOU LOSE');
+        } else {
+            alert('IT\'S A TIE');
+        };
+        displayToggle('#set2');
+        pS = 0, cS = 0;
+        cCh.innerHTML = 'Comp choice will appear here';
+        outcome.innerHTML = 'Round outcome here';
+        return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
     };
-    return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
 }
 
-function scissors() {
-    let comp = compChoice();
-    if (comp == 'rock') {
-        cCh.innerHTML = 'Comp chose rock!';
-        outcome.innerHTML = 'You lose';
-        cS++;
-    } else if (comp == 'paper') {
-        cCh.innerHTML = 'Comp chose paper!';
-        outcome.innerHTML = 'You win';
-        pS++;
+function paper(y) {
+    if (current < y) {
+        let comp = compChoice();
+        if (comp == 'rock') {
+            cCh.innerHTML = 'Comp chose rock!';
+            outcome.innerHTML = 'You win';
+            pS++;
+        } else if (comp == 'paper') {
+            cCh.innerHTML = 'Comp chose paper!';
+            outcome.innerHTML = 'It\'s a tie';
+        } else {
+            cCh.innerHTML = 'Comp chose scissors';
+            outcome.innerHTML = 'You lose';
+            cS++;
+        };
+        return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
     } else {
-        cCh.innerHTML = 'Comp chose scissors';
-        outcome.innerHTML = 'It\'s a tie';
-    };
-    return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
+        if (pS > cS) {
+            alert('YOU WIN');
+        } else if (pS < cS) {
+            alert('YOU LOSE');
+        } else {
+            alert('IT\'S A TIE');
+        };
+        displayToggle('#set2');
+        pS = 0, cS = 0;
+        cCh.innerHTML = 'Comp choice will appear here';
+        outcome.innerHTML = 'Round outcome here';
+        return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
+    }
+}
+
+function scissors(y) {
+    if (current < y) {
+        let comp = compChoice();
+        if (comp == 'rock') {
+            cCh.innerHTML = 'Comp chose rock!';
+            outcome.innerHTML = 'You lose';
+            cS++;
+        } else if (comp == 'paper') {
+            cCh.innerHTML = 'Comp chose paper!';
+            outcome.innerHTML = 'You win';
+            pS++;
+        } else {
+            cCh.innerHTML = 'Comp chose scissors';
+            outcome.innerHTML = 'It\'s a tie';
+        };
+        return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
+    } else {
+        if (pS > cS) {
+            alert('YOU WIN');
+        } else if (pS < cS) {
+            alert('YOU LOSE');
+        } else {
+            alert('IT\'S A TIE');
+        };
+        displayToggle('#set2');
+        pS = 0, cS = 0;
+        cCh.innerHTML = 'Comp choice will appear here';
+        outcome.innerHTML = 'Round outcome here';
+        return score.innerHTML = `Player: ${pS} || Comp: ${cS}`;
+    }
 }
 
 btn1.addEventListener('click', () => {
     displayToggle('#set2');
+    setLimit(1);
 });
 
 btn2.addEventListener('click', () => {
     displayToggle('#set2');
+    setLimit(3);
 });
 
 btn3.addEventListener('click', () => {
     displayToggle('#set2');
+    setLimit(5);
 });
 
 btnR.addEventListener('click', () => {
-    rock();
-    roundTicker();
+    rock(limit);
+    roundTicker(limit);
 })
 btnP.addEventListener('click', () => {
-    paper();
-    roundTicker();
+    paper(limit);
+    roundTicker(limit);
 })
 btnSc.addEventListener('click', () => {
-    scissors();
-    roundTicker();
+    scissors(limit);
+    roundTicker(limit);
+})
+
+checkerBtn.addEventListener('click', () => {
+    results();
 })
